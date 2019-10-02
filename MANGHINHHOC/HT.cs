0566c684a1ba709e845e200f724ebbf0b502fd03
do hoang huy <dohoanghuy280999@gmail.com>
2019-10-02 14:32:09 +0700

    Add project files.

---
+++ MANGHINHHOC/HT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MANGHINHHOC
+{
+    class HT:hinhhoc
+        private float Bk;
+        private const float PI = 3,14F;
+        public float bk
+    { 
+            get { return bk;}
+            set { bk = value;}
+    } 
+    {public hinhtron(float stBK)
+    { 
+        bk = stBK;
+    }
+        public override float DT()
+{
+ 	return base.DT + bk * bk * 3.14F
+}
+    }
+}
---
+++ MANGHINHHOC/HV.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MANGHINHHOC
+{
+    class HV:hinhhoc
+    public hinhvuong(float st(canh) : base()
+
+    {
+        canh =stcanh;
+    }
+    public float    canh
+{ get { return canh;}
+    set { canh = value; }
+}
+    public override float CV()
+{ return canh *  2 * 2;
+}
+    public override float DT()
+{ return canh * canh;
+}
+}
